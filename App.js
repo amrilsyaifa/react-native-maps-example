26b927177637f23b4b77d8826626124a3147682b
amril syaifa yasin <amril@mindaperdana.com>
2019-06-20 22:42:47 +0700

    add react-native-google-places-autocomplete
    exit

--- App.js
+++ App.js
@@ -9,30 +9,47 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }}
-        >
-          <MapView.Marker
-            coordinate={{
+      <View>
+        <View style={styles.container}>
+          <MapView
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            region={{
               latitude: 37.78825,
               longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
             }}
-            title="Lokasi"
-            description="Hello" />
-        </MapView>
+          >
+            <MapView.Marker
+              coordinate={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+              }}
+              title="Lokasi"
+              description="Hello" />
+          </MapView>
+        </View>
+        <View>
+          <GooglePlacesAutocomplete
+            placeholder='Enter Location'
+            minLength={2}
+            autoFocus={false}
+            fetchDetails
+            listViewDisplayed='auto'
+            query={{
+              key: 'AIzaSyCUVwuXUNjzldkfVLblLw37EVA5xjspvjI',
+              language: 'en',
+              types: 'geocode',
+            }}
+            currentLocation={false} />
+        </View>
       </View>
     );
   }
